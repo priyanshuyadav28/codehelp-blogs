3357adc781b2ea7fdc87abbd4fcb2253cacce64e
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2023-09-23 02:33:07 +0530

    routes added for components

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ import Pagination from "./components/Pagination";
 import { AppContext } from "./context/AppContext";
 import { useContext } from "react";
 import './App.css'
+import { Routes, Route, useSearchParams, useLocation } from "react-router-dom";
+import { findByPlaceholderText } from "@testing-library/react";
+import BlogPage from "./Pages/BlogPage";
+import TagPage from "./Pages/TagPage";
+import CategoryPage from "./Pages/CategoryPage";
+import Home from "./Pages/Home";
+
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
 
+  const [searchParams, setSearchParms] = useSearchParams();
+  const location = useLocation();
+
+
   useEffect(() => {
-    fetchBlogPosts();
-  }, []);
+    const page = searchParams("page") ?? 1;
+
+    // check if the url contains tags
+    if (location.pathname.includes("tags")) {
+      // iska matlab tag wala page show krna hai 
+      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchBlogPosts(Number(page), tag);
+    }
+    // check if the url contains categories
+    else if(location.pathname.includes("categories")) {
+      const category = location.pathname.split("/").at(-1).replaceAll("-", " ");
+      fetchBlogPosts(Number(page), null, category);
+    }
+    else {
+      fetchBlogPosts(Number(page));
+    }
+  }, [location.pathname, location.search]);
+
 
   return (
-    <div className="w-full h-full flex flex-col gap-1 justify-center items-center">
-      <Header />
-      <Blogs />
-      <Pagination />
-    </div>
+    <Routes>
+      <Route path="/" element = {<Home />} />
+      <Route path="/blog/:blogId" element = {<BlogPage/>} />
+      <Route path="/tags/:tag" element = {<TagPage/>} />
+      <Route path="/categories/:category" element = {<CategoryPage/>} />
+    </Routes>
   );
 }
